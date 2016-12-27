0e2d8cda8e235a9c1770542c07cea0a9daae45a6
shahar <shahar.amit111@gmail.com>
2016-12-27 20:08:28 +0200

    new screens

---
+++ includes/all.php
@@ -0,0 +1,11 @@
+<?php
+function converter_currency($amount, $from,$to){
+    $result = file_get_contents('https://www.google.com/finance/converter?a='.$amount.'&from='.$from.'&to='.$to);
+    preg_match('#\<span class=bld\>(.+?)\<\/span\>#s', $result, $finalData);
+    return $finalData[1];
+}
+
+echo converter_currency(5, 'EURO','ILS'); // 5.4125 USD
+
+list($amount, $currency) = explode(' ', converter_currency(5, 'EUR','ILS'));
+var_dump($amount, $currency); // string(6) "5.4125" string(3) "USD"
