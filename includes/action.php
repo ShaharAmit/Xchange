8a2e62f0a3e88b4cb75b453ed4fc5d9033cba96d
shahar <shahar.amit111@gmail.com>
2017-02-11 12:31:25 +0200

    update

--- includes/action.php
+++ includes/action.php
@@ -1,54 +1,33 @@
 <?php
+include('db.php');
 if ($_SERVER["REQUEST_METHOD"] == "GET") {
+    $arr = array();
     if($_GET["action"] == "data") {
-        include('db.php');
-        $query = "SELECT * FROM tbl_users_234 WHERE I < 6";
-        $result = mysqli_query($connection, $query);
 
-        /*$row=mysqli_fetch_assoc($result);
-        $arr = array ('ID'=>$row["ID"],'FIRST_NAME'=>$row["FIRST_NAME"],'LAST_NAME'=>$row["LAST_NAME"]);
-        header('Content-type: application/json');
-        echo json_encode($arr);*/
+        $cards = $_GET["cards"];
+        $cardsEnd = $cards +1;
 
-        $arr = array();
+        $stmt = $connection->prepare("SELECT * FROM tbl_users_234 WHERE I BETWEEN ? AND ?");
+        $stmt->bind_param('ii', $cards, $cardsEnd);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $stmt->close();
 
-        if ($result->num_rows > 0) {
-            while ($row = mysqli_fetch_assoc($result)) {
-                /*echo "id: " . $row["ID"] . " - Name: " . $row["FIRST_NAME"] . " " . $row["LAST_NAME"] . "<br>";*/
-                $arr[] = $row;
-            }
-        }
-        header('Content-type: application/json');
-        print json_encode($arr);
-        mysqli_free_result($result);
+
+    } elseif ($_GET["action"] == "count"){
+        $query = "SELECT COUNT(*) FROM tbl_users_234";
+        $result = mysqli_query($connection,$query);
     }
-}
-    $connection->close();
 
-
-/*if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $class = $_POST["ClassNumber"];
-    $building = $_POST["Building"];
-    $floor = $_POST["Floor"];
-
-    if($_GET["action"] == "s") {
-
-        // Create connection
-        $con=mysqli_connect("$servername","$username","$password","$dbname");
-        // Check connection
-        if (mysqli_connect_errno())
-        {
-            echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    if ($result->num_rows > 0) {
+        while ($row = mysqli_fetch_assoc($result)) {
+            $arr[] = $row;
         }
+    }
 
-        $sql="SELECT * FROM CLASS_TABLE WHERE CLASS = $class";
-        $result=mysqli_query($con,$sql);
-        // Associative array
-        $row=mysqli_fetch_assoc($result);
-        $arr = array ('ClassNumber'=>$row["CLASS"],'Floor'=>$row["FLOOR"],'Building'=>$row["BUILDING"]);
-        header('Content-type: application/json');
-        echo json_encode($arr);
-        // Free result set
-        mysqli_free_result($result);
-        mysqli_close($con);
-    }*/
+    header('Content-type: application/json');
+    print json_encode($arr);
+
+    mysqli_free_result($result);
+    $connection->close();
+}
--- includes/home.js
+++ includes/home.js
@@ -2,15 +2,14 @@ $(function() {
     var obj=$(".clear").last(),
         cont=$(".cont").last(),
         cards=0,
-        i,
         fName,
         lName,
-        id,
         deals,
         likes,
         disLikes,
         pictureURL,
-        rankURL;
+        rankURL,
+        rowsNum;
     $("a[href='../profile/index.html']").attr("href","./screens/profile/index.html");
     $("a[href='../meeting/index.html']").attr("href","./screens/meeting/index.html");
     $("a[href='../messages/index.html']").attr("href","./screens/messages/index.html");
@@ -18,93 +17,129 @@ $(function() {
     $("a[href='../buying/index.html']").attr("href","./screens/buying/index.html");
     $("a[href='../../index.html']").attr("href","./index.html");
     cont.before( "<div class='topLine'></div>" +
-                "<div class='clear'></div>" +
-                "<h1>מוצאים - מחליפים - חוסכים</h1>" +
-                "<div class='bottomLine'></div>" +
-                "<div class='clear'></div>"
+        "<div class='clear'></div>" +
+        "<h1>מוצאים - מחליפים - חוסכים</h1>" +
+        "<div class='bottomLine'></div>" +
+        "<div class='clear'></div>"
     );
-    /*for(i=0;i<24;i++){
-        InSection();
-    }
-    function InSection(){
+    dataCall();
+    $(function () {
+        $.ajax({
+            type: "GET",
+            url: "includes/action.php?action=count",
+            dataType: 'json',
+            success: function (data) {
+                $.each(data, function(index, element) {
+                    rowsNum = element["COUNT(*)"];
+                });
+            }
+        });
+    });
+    function AddRight() {
         obj.before("<section class='cardsRight'>" +
-                        "<p>בר ביצע 2 עסקאות</p>" +
-                        "<section class='avatars'>" +
-                            "<section class='score'></section>" +
-                        "</section>" +
-                        "<section class='likes'>" +
-                            "<section class='like'></section>" +
-                            "<p>0</p>" +
-                            "<section class='disLike'></section>" +
-                            "<p>2</p>" +
-                        "</section>" +
-                    "</section>" +
-                    "<section class='cardsLeft'>" +
-                        "<p>בר ביצע 2 עסקאות</p>" +
-                        "<section class='avatars'>" +
-                            "<section class='score'></section>" +
-                        "</section>" +
-                        "<section class='likes'>" +
-                            "<section class='like'></section>" +
-                            "<p>0</p>" +
-                            "<section class='disLike'></section>" +
-                            "<p>2</p>" +
-                        "</section>" +
-                    "</section>");
-        cards++;
-    }*/
-    function AddRight(index) {
-        obj.before("<section class='cardsRight'>" +
-                        "<p>"+fName+" "+lName+ " - "+deals+" עסקאות"+"</p>" +
-                        "<section class='avatars'>" +
-                            "<section class='score'></section>" +
-                        "</section>" +
-                        "<section class='likes'>" +
-                            "<section class='like'></section>" +
-                            "<p>"+likes+"</p>" +
-                            "<section class='disLike'></section>" +
-                            "<p>"+disLikes+"</p>" +
-                        "</section>" +
-                    "</section>");
-        $(".score").eq(index).css('background-image', 'url(' + rankURL + ')');
-        $(".avatars").eq(index).css('background-image', 'url(' + pictureURL + ')');
+            "<p>"+fName+" "+lName+ " - "+deals+" עסקאות"+"</p>" +
+            "<section class='avatars'>" +
+            "<section class='score'></section>" +
+            "</section>" +
+            "<section class='likes'>" +
+            "<section class='like'></section>" +
+            "<p>"+likes+"</p>" +
+            "<section class='disLike'></section>" +
+            "<p>"+disLikes+"</p>" +
+            "</section>" +
+            "</section>");
+        $(".score").eq(cards).css('background-image', 'url(' + rankURL + ')');
+        $(".avatars").eq(cards).css('background-image', 'url(' + pictureURL + ')');
         cards++;
     }
-    function AddLeft(index) {
+    function AddLeft() {
         obj.before("<section class='cardsLeft'>" +
-                        "<p>"+fName+" "+lName+ " - "+deals+" עסקאות"+"</p>" +
-                        "<section class='avatars'>" +
-                            "<section class='score'></section>" +
-                        "</section>" +
-                        "<section class='likes'>" +
-                            "<section class='like'></section>" +
-                            "<p>"+likes+"</p>" +
-                            "<section class='disLike'></section>" +
-                            "<p>"+disLikes+"</p>" +
-                        "</section>" +
-                    "</section>");
-        $(".score").eq(index).css('background-image', 'url(' + rankURL + ')');
-        $(".avatars").eq(index).css('background-image', 'url(' + pictureURL + ')');
+            "<p>"+fName+" "+lName+ " - "+deals+" עסקאות"+"</p>" +
+            "<section class='avatars'>" +
+            "<section class='score'></section>" +
+            "</section>" +
+            "<section class='likes'>" +
+            "<section class='like'></section>" +
+            "<p>"+likes+"</p>" +
+            "<section class='disLike'></section>" +
+            "<p>"+disLikes+"</p>" +
+            "</section>" +
+            "</section>");
+        $(".score").eq(cards).css('background-image', 'url(' + rankURL + ')');
+        $(".avatars").eq(cards).css('background-image', 'url(' + pictureURL + ')');
         cards++;
     }
-    $.ajax({
-        type: "GET",
-        url: "includes/action.php?action=data",
-        dataType: 'json',
-        success: function (data) {
-            $.each(data, function(index, element) {
-                fName = element.FIRST_NAME;
-                lName = element.LAST_NAME;
-                deals = element.DEALS_AMOUNT;
-                likes = element.LIKES;
-                disLikes = element.DISLIKES;
-                pictureURL = element.PICTURE;
-                rankURL = element.RANK;
-                if(index%2 == 0)
-                    AddRight(index);
-                else
-                    AddLeft(index);
-            });
+    function dataCall() {
+        obj.before("<i class='fa fa-refresh fa-spin' style='font-size:60px; position: fixed; left: 50%; top: 50%'></i>");
+        disableScroll();
+        $.ajax({
+            type: "GET",
+            url: "includes/action.php?",
+            data:{
+                action: "data",
+                cards: cards+1
+            },
+            dataType: 'json',
+            success: function (data) {
+                $.each(data, function(index, element) {
+                    fName = element.FIRST_NAME;
+                    lName = element.LAST_NAME;
+                    deals = element.DEALS_AMOUNT;
+                    likes = element.LIKES;
+                    disLikes = element.DISLIKES;
+                    pictureURL = element.PICTURE;
+                    rankURL = element.RANK;
+                    if(index%2 == 0)
+                        AddRight();
+                    else
+                        AddLeft();
+                });
+                enableScroll();
+                $(".fa").remove();
+            }
+        });
+    }
+    $(window).on("scroll", function() {
+        var scrollHeight = $(document).height();
+        var scrollPosition = $(window).height() + $(window).scrollTop();
+        if ((scrollHeight - scrollPosition) / scrollHeight === 0) {
+            if(cards<rowsNum)
+                dataCall();
         }
     });
+    // left: 37, up: 38, right: 39, down: 40,
+    // spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
+    var keys = {37: 1, 38: 1, 39: 1, 40: 1};
+
+    function preventDefault(e) {
+        e = e || window.event;
+        if (e.preventDefault)
+            e.preventDefault();
+        e.returnValue = false;
+    }
+
+    function preventDefaultForScrollKeys(e) {
+        if (keys[e.keyCode]) {
+            preventDefault(e);
+            return false;
+        }
+    }
+
+    function disableScroll() {
+        if (window.addEventListener) // older FF
+            window.addEventListener('DOMMouseScroll', preventDefault, false);
+        window.onwheel = preventDefault; // modern standard
+        window.onmousewheel = document.onmousewheel = preventDefault; // older browsers, IE
+        window.ontouchmove  = preventDefault; // mobile
+        document.onkeydown  = preventDefaultForScrollKeys;
+    }
+
+    function enableScroll() {
+        if (window.removeEventListener)
+            window.removeEventListener('DOMMouseScroll', preventDefault, false);
+        window.onmousewheel = document.onmousewheel = null;
+        window.onwheel = null;
+        window.ontouchmove = null;
+        document.onkeydown = null;
+    }
 });
