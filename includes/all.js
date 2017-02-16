99712262c3de379e0637bfca19d73d728e4af385
shahar <shahar.amit111@gmail.com>
2017-02-16 17:13:24 +0200

    update

--- includes/all.js
+++ includes/all.js
@@ -1,37 +1,28 @@
 $(function(){
-    var $header = $("body");
+    var $header = $("body"),
+        $nav = $("header > button");
     $header.prepend(
         "<header>" +
             "<div><a id='profile' href='../profile/index.html'></a></div>" +
-            "<nav>" +
-                "<ul class='nav nav-pills'>" +
-                    "<li role='presentation'><a href='../meeting/index.html'>עסקאות" +
-                        "<div class='messages'><p>10</p></div>" +
-                    "</a></li>" +
-                    "<li role='presentation'><a href='../messages/index.html'>הודעות" +
-                        "<div class='meeting'><p>1</p></div>" +
-                    "</a></li>" +
-                "</ul>" +
-                "<ul class='nav nav-pills'>" +
-                    "<li role='presentation'><a href='../selling/index.html'>מכירת מט&#8221;ח</a></li>" +
-                    "<li role='presentation'><a href='../buying/index.html'>קניית מט&#8221;ח</a></li>" +
-                "</ul>" +
-                "<a id='logo' href='../../index.html'></a>" +
-            "</nav>" +
-        "</header>"
+            "<button></button>" +
+            "<a id='logo' href='../../index.html'></a>" +
+        "</header>" +
+        "<nav>" +
+            "<ul class='nav nav-pills' id='ul1'>" +
+                "<li role='presentation'><a href='../meeting/index.html'>עסקאות" +
+                    "<div class='messages'><p>10</p></div>" +
+                "</a></li>" +
+                "<li role='presentation'><a href='../messages/index.html'>הודעות" +
+                    "<div class='meeting'><p>1</p></div>" +
+                "</a></li>" +
+            "</ul>" +
+            "<ul class='nav nav-pills' id='ul2'>" +
+                "<li role='presentation'><a href='../selling/index.html'>מכירת מט&#8221;ח</a></li>" +
+                "<li role='presentation'><a href='../buying/index.html'>קניית מט&#8221;ח</a></li>" +
+            "</ul>" +
+        "</nav>"
     );
-    var $li = $("li");
-    $li.eq(0).css({"float":"left","margin-left":"10vw"});
-    $li.eq(1).css({"float":"right"});
-    $li.eq(3).css({"float":"right","margin-right":"10vw"});
-    var checkWidth = (function(){
-        if ($(window).width() < 1000) {
-            $li.eq(0).css({"float":"left","margin-left":"2vw"});
-            $li.eq(3).css({"float":"right","margin-right":"2vw"});
-        }else if($(window).width() > 1000){
-            $li.eq(0).css({"float":"left","margin-left":"10vw"});
-            $li.eq(3).css({"float":"right","margin-right":"10vw"});
-        }
+    $nav.click(function () {
+
     });
-    $(window).resize(checkWidth);
 });
